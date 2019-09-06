db65fe984419acfd5b9dbbe15198cacba5fb4de9
Dima Po <dima.djunaev@gmail.com>
2019-09-06 11:53:42 +0500

    Initial

---
+++ gulpfile.babel.js
@@ -0,0 +1,84 @@
+"use strict";
+
+import gulp from "gulp";
+
+const requireDir = require("require-dir"),
+    paths = {
+        views: {
+            src: [
+                "./src/views/index.pug",
+                "./src/views/pages/*.pug"
+            ],
+            dist: "./123041-device-26/",
+            watch: [
+                "./src/blocks/**/*.pug",
+                "./src/views/**/*.pug"
+            ]
+        },
+        styles: {
+            src: "./src/styles/main.{scss,sass}",
+            dist: "./123041-device-26/css/",
+            watch: [
+                "./src/blocks/**/*.{scss,sass}",
+                "./src/styles/**/*.{scss,sass}"
+            ]
+        },
+        scripts: {
+            src: "./src/js/index.js",
+            dist: "./123041-device-26/js/",
+            watch: [
+                "./src/blocks/**/*.js",
+                "./src/js/**/*.js"
+            ]
+        },
+        images: {
+            src: [
+                "./src/img/**/*.{jpg,jpeg,png,gif,tiff,svg}",
+                "!./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}"
+            ],
+            dist: "./123041-device-26/img/",
+            watch: "./src/img/**/*.{jpg,jpeg,png,gif,svg}"
+        },
+        webp: {
+            src: [
+                "./src/img/**/*.{jpg,jpeg,png,tiff}",
+                "!./src/img/favicon/*.{jpg,jpeg,png,gif}"
+            ],
+            dist: "./123041-device-26/img/",
+            watch: [
+                "./src/img/**/*.{jpg,jpeg,png,tiff}",
+                "!./src/img/favicon.{jpg,jpeg,png,gif}"
+            ]
+        },
+        sprites: {
+            src: "./src/img/svg/*.svg",
+            dist: "./123041-device-26/img/sprites/",
+            watch: "./src/img/svg/*.svg"
+        },
+        fonts: {
+            src: "./src/fonts/**/*.{woff,woff2}",
+            dist: "./123041-device-26/fonts/",
+            watch: "./src/fonts/**/*.{woff,woff2}"
+        },
+        favicons: {
+            src: "./src/img/favicon/*.{jpg,jpeg,png,gif,tiff}",
+            dist: "./123041-device-26/img/favicons/",
+        },
+        gzip: {
+            src: "./src/.htaccess",
+            dist: "./123041-device-26/"
+        }
+    };
+
+requireDir("./gulp-tasks/");
+
+export { paths };
+
+export const development = gulp.series("clean", "smart-grid",
+    gulp.parallel(["views", "styles", "scripts", "images", "webp", "sprites", "fonts", "favicons"]),
+    gulp.parallel("serve"));
+
+export const prod = gulp.series("clean",
+    gulp.series(["views", "styles", "scripts", "images", "webp", "sprites", "fonts", "favicons", "gzip"]));
+
+export default development;
