b1a271f327e89f4dc403ad26559855dee56e2945
Dima Po <dima.djunaev@gmail.com>
2019-09-08 13:18:46 +0500

    Updated

---
+++ src/blocks/modules/hp-slider/hp-slider.js
@@ -0,0 +1,13 @@
+modules.define('hp-slider', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
