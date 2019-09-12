9016df5a5f59fd8b3ea6d7b87754185a87e2bbeb
Dima Po <dima.djunaev@gmail.com>
2019-09-12 20:44:01 +0500

    Updated

--- gulp-tasks/views.js
+++ gulp-tasks/views.js
@@ -7,6 +7,7 @@ import gulpif from "gulp-if";
 import replace from "gulp-replace";
 import browsersync from "browser-sync";
 import yargs from "yargs";
+import htmlbeautify from "gulp-html-beautify";
 
 const argv = yargs.argv,
     production = !!argv.production;
@@ -14,10 +15,11 @@ const argv = yargs.argv,
 gulp.task("views", () => {
     return gulp.src(paths.views.src)
         .pipe(pug({
-            pretty: true
+            // pretty: true
         }))
         .pipe(gulpif(production, replace(".css", ".min.css")))
         .pipe(gulpif(production, replace(".js", ".min.js")))
+        .pipe(htmlbeautify())
         .pipe(gulp.dest(paths.views.dist))
         .pipe(browsersync.stream());
 });
