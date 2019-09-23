613c4e475fdc2669f19896d46da4ebba17207926
Dima Po <dima.djunaev@gmail.com>
2019-09-23 17:24:39 +0500

    Сделал разметку шапки

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -3,5 +3,14 @@ document.getElementsByClassName("header__secondary-menu-search-input")[0].addEve
 });
 
 document.getElementsByClassName("header__secondary-menu-search-input")[0].addEventListener("blur", function() {
-    document.getElementsByClassName("header__secondary-menu-search-btn")[0].removeAttribute("style", "visibility: visible");
+    document.getElementsByClassName("header__secondary-menu-search-btn")[0].removeAttribute("style", "visibility: hidden");
 });
+
+document.querySelector(".header__main-menu-item--dropdown").addEventListener("click", function(e){
+    e.preventDefault();
+	document.querySelector(".header__main-menu-submenu").classList.toggle("header__main-menu-submenu--active");
+});
+
+// document.querySelector(".header__main-menu-item--dropdown").addEventListener("mouseout", function(){
+// 	document.querySelector(".header__main-menu-submenu").classList.remove("header__main-menu-submenu--active");
+// });
