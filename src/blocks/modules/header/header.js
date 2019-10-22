826688e3d789358d752f9557726f79b58a704d3a
Dima Po <dima.djunaev@gmail.com>
2019-10-22 09:56:04 +0500

    Fix

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,10 +1,10 @@
-document.getElementsByClassName("header__secondary-menu-search-input")[0].addEventListener("focus", function() {
-    document.getElementsByClassName("header__secondary-menu-search-btn")[0].setAttribute("style", "visibility: visible");
-});
+// document.getElementsByClassName("header__secondary-menu-search-input")[0].addEventListener("focus", function() {
+//     document.getElementsByClassName("header__secondary-menu-search-btn")[0].setAttribute("style", "visibility: visible");
+// });
 
-document.getElementsByClassName("header__secondary-menu-search-input")[0].addEventListener("blur", function() {
-    document.getElementsByClassName("header__secondary-menu-search-btn")[0].removeAttribute("style", "visibility: hidden");
-});
+// document.getElementsByClassName("header__secondary-menu-search-input")[0].addEventListener("blur", function() {
+//     document.getElementsByClassName("header__secondary-menu-search-btn")[0].removeAttribute("style", "visibility: hidden");
+// });
 
 document.querySelector(".header__main-menu-item--dropdown").addEventListener("click", function(e){
     // e.preventDefault();
